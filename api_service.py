50f3dc18bf9908bf735c127c7b1c57beec05f7b1
Mouloud <mouloud.bellil@outlook.fr>
2025-09-30 19:53:59 +0200

    ajout fonctions permettants récupérées données API

--- api_service.py
+++ api_service.py
@@ -1,33 +1,47 @@
-# Tous les appels à l'API
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import requests
-import json
 
 from api_config import BASE_URL
 
-def coindesk_api_get_echanges_rates():
+
+def get_dates_intervals(start_date: datetime, end_date: datetime, max_days: int):
+    diff_days = (end_date - start_date).days
+    dates_interval = []
+    interval_begin_date = start_date
+    while diff_days > 0:
+        nb_days_to_add = max_days - 1
+        interval_ending_date = interval_begin_date + timedelta(nb_days_to_add)
+        
+        if diff_days <= max_days:
+            interval_ending_date = end_date
+   
+        dates_interval.append([interval_begin_date, interval_ending_date])
+        interval_begin_date = interval_ending_date + timedelta(1)
+        diff_days -= max_days
+    return dates_interval
+        
+
+def binance_api_get_echanges_rate(currency: str, start_date: datetime, end_date: datetime):
     url = BASE_URL + "api/v3/klines"
     params = {
-        "symbol": "BTCEUR",
-        "interval": "1d",  # 1 jour
-        "startTime": int(datetime(2021, 1, 1).timestamp() * 1000),
-        "endTime": int(datetime(2021, 1, 11).timestamp() * 1000),
-        "limit": 10
+        "symbol": currency,
+        "interval": "1d",
+        "startTime": int(start_date.timestamp() * 1000),
+        "endTime": int(end_date.timestamp() * 1000)
     }
-
     response = requests.get(url, params=params)
-
     if response.status_code != 200:
-        print(f"Error during HTPP request: {response.status_code}")
+        print(f"Error during HTTP request: {response.status_code}")
         return None
-    else:
-        json_response = response.json()
-        price_close = []
-        for r in json_response:
-            timestamp = r[0] / 1000
-            date = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")
-            close_price = float(r[4])
-            
-            price_close.append((date, close_price))
-        return price_close
     
+    data = response.json()
+    return data
+
+
+def binance_api_get_echanges_rate_extended(currency, start_date: datetime, end_date: datetime):
+    all_data = []
+    dates_intervals = get_dates_intervals(start_date, end_date, 500)   
+    if dates_intervals:
+        for interval in dates_intervals:
+            all_data += binance_api_get_echanges_rate(currency, interval[0], interval[1])
+    return all_data
--- main.py
+++ main.py
@@ -1,8 +1,77 @@
-from api_service import coindesk_api_get_echanges_rates
+import json
+from pathlib import Path
+from typing import List
+
+from api_service import binance_api_get_echanges_rate, binance_api_get_echanges_rate_extended, get_dates_intervals
+from datetime import datetime, timedelta, date
+
+number_of_days = 3
+
+end_date = datetime.today()
+start_date = datetime(2025, 9, 28)
+
+asset = "BTCEUR"
+filename = asset + ".json"
 
 
-if __name__ == "__main__":
-    rates = coindesk_api_get_echanges_rates()
-    print(f"{'Date'}: {'Price close (€)'}")
-    for rate in rates:
-        print(f"{rate[0]}: {rate[1]}")
+def load_json_data_from_file(filename):
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+def extract_essential_data(row_data: List) -> List:
+    data = []
+    for d in row_data:
+        date_obj = datetime.fromtimestamp(d[0] / 1000)
+        data.append({"date": date_obj.strftime("%Y/%m/%d"), "value": d[4]})
+    return data
+     
+
+def save_data_to_json(filename, data): 
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+        
+
+
+
+# data = []
+# if Path(filename).exists():
+#     data = load_json_data_from_file(filename)
+    
+# if data:
+#     saved_start_date = datetime.strptime(data[0]["date"], "%Y/%m/%d")
+#     saved_end_date = datetime.strptime(data[-1]["date"], "%Y/%m/%d")
+    
+#     apres = False
+#     if start_date < saved_start_date:
+#         end_date = saved_start_date
+       
+#     elif start_date > saved_end_date:
+#         start_date = saved_end_date + timedelta(1)
+#         apres = True
+#     else:
+#         start_date = saved_end_date + timedelta(1)
+#         apres = True
+        
+#     row_data = binance_api_get_echanges_rate(asset, start_date, end_date)
+#     new_data = extract_essential_data(data)
+#     if apres:
+#         data += new_data
+#     else:
+#         data = new_data + data 
+#     save_data_to_json(filename, data)
+    
+# else:
+#     row_data = binance_api_get_echanges_rate(asset, start_date, end_date)
+#     data = extract_essential_data(row_data)
+#     save_data_to_json(filename, data)
+
+
+row_data = binance_api_get_echanges_rate_extended(datetime(2024, 1, 1), datetime(2024, 12, 31))
+
+print(len(row_data))
+
+
+
+    
+
