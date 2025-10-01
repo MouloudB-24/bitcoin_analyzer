0a7e4a6f8f25493b0d729567b156c67fcd8770ec
Mouloud <mouloud.bellil@outlook.fr>
2025-10-01 18:03:16 +0200

    ajout algo pour déterminer points achats/ventes de BTC

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from rates_data_manage import get_and_manage_rates_data
 import matplotlib.pyplot as plt
 
-from rates_data_processing import compute_moving_average_for_rates_data
+from rates_data_processing import compute_buy_and_sell_points_from_ma, compute_moving_average_for_rates_data
 
 start_date = datetime(2025, 1, 1)
 end_date = datetime(2025, 10, 1)
@@ -14,12 +14,14 @@ print("Nombre de cours d'echanges BTC en EURO :", len(data))
 m100 = compute_moving_average_for_rates_data(data, 100)
 m20 = compute_moving_average_for_rates_data(data, 20)
 
-ma_intervals = [10, 50]
+ma_values = [10, 50]
 ma_lists = []
 
-for v in ma_intervals:
-    ma = compute_moving_average_for_rates_data(data, v)
-    ma_lists.append((ma, v))
+for value in ma_values:
+    ma_list = compute_moving_average_for_rates_data(data, value)
+    ma_lists.append((ma_list, value))
+
+buy_and_sell_points = compute_buy_and_sell_points_from_ma(ma_lists[0][0],ma_lists[1][0], 2)
 
 # # Afficher graph
 rates_dates = [datetime.strptime(d["date"], "%Y/%m/%d") for d in data]
@@ -31,6 +33,10 @@ plt.plot(rates_dates, rates_values)
 for ma_items in ma_lists:
     ma_values = [r["value"] for r in ma_items[0]]
     plt.plot(rates_dates, ma_values, label=f"M{ma_items[1]}")
+
+for point in buy_and_sell_points:
+    date_obj = datetime.strptime(point[0], "%Y/%m/%d")
+    plt.axvline(x=date_obj, color="r" if point[1] else "y")
 
 plt.legend()
 plt.show()
--- rates_data_processing.py
+++ rates_data_processing.py
@@ -15,6 +15,23 @@ def compute_moving_average_for_rates_data(rates, nb_days_interval):
             sum += - rates[i-nb_days_interval]["value"]
         averages.append({"date": rates[i]["date"], "value": sum / divisor})
     return averages
-        
-        
-        
+
+
+
+def compute_buy_and_sell_points_from_ma(short_ma, long_ma, threshold_percent=0):
+    buy = True
+    points = []
+    for i in range(len(short_ma)):
+        date_str = short_ma[i]["date"]
+        sma_value = short_ma[i]["value"]
+        lma_value = long_ma[i]["value"]
+        muti = 1 + threshold_percent/100
+        if buy:
+            if sma_value > lma_value*muti:
+                points.append((date_str, buy))
+                buy = False
+        else:
+            if sma_value < lma_value/muti:
+                points.append((date_str, buy))
+                buy = True
+    return points
