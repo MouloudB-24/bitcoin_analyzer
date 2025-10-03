0776cd7a9c2e6391e8bbaa3af9d02cdf6c00da33
Mouloud <mouloud.bellil@outlook.fr>
2025-10-03 22:02:18 +0200

    ajout fonction qui calcul points achats/ventes & afficher resultats

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from datetime import datetime
 from rates_data_manage import get_and_manage_rates_data
 import matplotlib.pyplot as plt
 
-from rates_data_processing import compute_buy_and_sell_points_from_ma, compute_moving_average_for_rates_data
+from rates_data_processing import compute_buy_and_sell_btc, compute_buy_and_sell_points_from_ma, compute_moving_average_for_rates_data
 
-start_date = datetime(2025, 1, 1)
-end_date = datetime(2025, 10, 1)
+start_date = datetime(2024, 10, 1)
+end_date = datetime(2025, 10, 3)
 currency = "BTCEUR"
 
+initial_wallet = 1000
+
 data = get_and_manage_rates_data(currency, start_date, end_date)
-print("Nombre de cours d'echanges BTC en EURO :", len(data))
+print("Nb rates :", len(data))
 
 m100 = compute_moving_average_for_rates_data(data, 100)
 m20 = compute_moving_average_for_rates_data(data, 20)
@@ -21,9 +23,15 @@ for value in ma_values:
     ma_list = compute_moving_average_for_rates_data(data, value)
     ma_lists.append((ma_list, value))
 
-buy_and_sell_points = compute_buy_and_sell_points_from_ma(ma_lists[0][0],ma_lists[1][0], 2)
+buy_and_sell_points = compute_buy_and_sell_points_from_ma(ma_lists[0][0],ma_lists[1][0], 1)
 
-# # Afficher graph
+final_wallet = compute_buy_and_sell_btc(initial_wallet, data, buy_and_sell_points)
+
+print(f"\nPériode achat et vente du BTC : {buy_and_sell_points[0][0]} -  {buy_and_sell_points[-1][0]}")
+print("Compte initial :", initial_wallet, "Euros")
+print("Compte final :", round(final_wallet), "Euros")
+
+# Afficher graph
 rates_dates = [datetime.strptime(d["date"], "%Y/%m/%d") for d in data]
 rates_values = [r["value"] for r in data]
 plt.ylabel(currency)
--- rates_data_processing.py
+++ rates_data_processing.py
@@ -1,5 +1,3 @@
-    
-
 
 def compute_moving_average_for_rates_data(rates, nb_days_interval):
     averages = []
@@ -15,7 +13,6 @@ def compute_moving_average_for_rates_data(rates, nb_days_interval):
             sum += - rates[i-nb_days_interval]["value"]
         averages.append({"date": rates[i]["date"], "value": sum / divisor})
     return averages
-
 
 
 def compute_buy_and_sell_points_from_ma(short_ma, long_ma, threshold_percent=0):
@@ -35,3 +32,42 @@ def compute_buy_and_sell_points_from_ma(short_ma, long_ma, threshold_percent=0):
                 points.append((date_str, buy))
                 buy = True
     return points
+
+
+def get_rate_value_for_date_str(date, rates):
+    for rate in rates:
+        if date == rate["date"]:
+            return rate["value"]
+    return None
+
+
+def compute_buy_and_sell_btc(initial_wallet, rates, buy_and_sell_points):
+    shares = 0
+    current_wallet = initial_wallet
+    last_wallet = 0
+    
+    if buy_and_sell_points[-1][1]:
+        buy_and_sell_points = buy_and_sell_points[:-1]
+    
+    for point in buy_and_sell_points:
+        current_date = point[0]
+        btc_value = get_rate_value_for_date_str(current_date, rates)
+        if btc_value:
+            if point[-1]:
+                print(f"Le {current_date} j'achete des BTC pour une valeur de {round(current_wallet)} Euros")
+                shares = current_wallet / btc_value
+                last_wallet = current_wallet
+                current_wallet = 0
+            else:
+                current_wallet = shares * btc_value
+                shares = 0
+                print(f"Le {current_date} je vends mes BTC. Et je récupère {round(current_wallet)} Euros")
+                
+                ratio = (current_wallet - last_wallet)/last_wallet*100
+                if ratio > 0:
+                    print(f"Soit un gain de {round(ratio, 1)}%\n")
+                else:
+                    print(f"Soit une perte de {round(ratio, 1)}%\n")
+        else:
+            print("Pas d'achat et/ou du vente de BTC")
+    return current_wallet
